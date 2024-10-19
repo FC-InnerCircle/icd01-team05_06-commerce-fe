7a5c5971ea7227bf7b6b12a81b1c614067a3a664
2scent <ad7549@gmail.com>
2024-10-19 16:49:57 +0900

    feat: 나의 리뷰 조회 TanStack Query 적용

---
+++ apps/commerce-client-web/src/app/(default)/me/reviews/components/reviews.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import Link from 'next/link';
+import { useQuery } from '@tanstack/react-query';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import StarRating from '@/components/common/star-rating';
+import { getMyReviews } from '@/app/actions/my-review-action';
+import DeleteAlertDialog from './delete-alert-dialog';
+
+const Reviews = () => {
+  const {
+    data: { reviews = [] },
+  } = useQuery({
+    queryKey: ['me', 'reviews'],
+    queryFn: getMyReviews,
+  });
+
+  return (
+    <div className="overflow-x-auto">
+      <Table className="table-fixed text-xs">
+        <TableHeader>
+          <TableRow className="text-center">
+            <TableHead className="hidden w-16 text-center md:table-cell">작성일자</TableHead>
+            <TableHead className="w-24 text-center">상품</TableHead>
+            <TableHead className="w-36 text-center">내용</TableHead>
+            <TableHead className="w-8 text-center">점수</TableHead>
+            <TableHead className="w-8 text-center">삭제</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {reviews.map((review) => (
+            <TableRow key={review.reviewId} className="text-center text-xs">
+              <TableCell className="hidden truncate md:table-cell">{review.createdAt}</TableCell>
+              <TableCell className="truncate">
+                <Link className="text-blue-700" href={`/details/${review.productId}`}>
+                  {review.productTitle}
+                </Link>
+              </TableCell>
+              <TableCell className="truncate">{review.content}</TableCell>
+              <TableCell className="truncate">
+                <StarRating rating={review.score} />
+              </TableCell>
+              <TableCell className="truncate">
+                <DeleteAlertDialog reviewId={review.reviewId} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default Reviews;
--- apps/commerce-client-web/src/app/(default)/me/reviews/page.tsx
+++ apps/commerce-client-web/src/app/(default)/me/reviews/page.tsx
@@ -1,55 +1,22 @@
-import Link from 'next/link';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
-import StarRating from '@/components/common/star-rating';
 import { getMyReviews } from '@/app/actions/my-review-action';
-import DeleteAlertDialog from './components/delete-alert-dialog';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import Reviews from '@/app/(default)/me/reviews/components/reviews';
 
 const Page = async () => {
-  const { reviews } = await getMyReviews();
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['me', 'reviews'],
+    queryFn: getMyReviews,
+  });
 
   return (
-    <div className="flex flex-col gap-4">
-      <header>나의 리뷰</header>
-      <div className="overflow-x-auto">
-        <Table className="table-fixed text-xs">
-          <TableHeader>
-            <TableRow className="text-center">
-              <TableHead className="hidden w-16 text-center md:table-cell">작성일자</TableHead>
-              <TableHead className="w-24 text-center">상품</TableHead>
-              <TableHead className="w-36 text-center">내용</TableHead>
-              <TableHead className="w-8 text-center">점수</TableHead>
-              <TableHead className="w-8 text-center">삭제</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {reviews.map((review) => (
-              <TableRow key={review.reviewId} className="text-center text-xs">
-                <TableCell className="hidden truncate md:table-cell">{review.createdAt}</TableCell>
-                <TableCell className="truncate">
-                  <Link className="text-blue-700" href={`/details/${review.productId}`}>
-                    {review.productTitle}
-                  </Link>
-                </TableCell>
-                <TableCell className="truncate">{review.content}</TableCell>
-                <TableCell className="truncate">
-                  <StarRating rating={review.score} />
-                </TableCell>
-                <TableCell className="truncate">
-                  <DeleteAlertDialog reviewId={review.reviewId} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex flex-col gap-4">
+        <header>나의 리뷰</header>
+        <Reviews />
       </div>
-    </div>
+    </HydrationBoundary>
   );
 };
 
