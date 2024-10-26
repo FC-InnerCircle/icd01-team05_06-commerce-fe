dad13967833b7f498a8ccd86d30c978b8a3c0fca
DahnBee Shim <colleksql@8snippet.com>
2024-10-26 15:41:53 +0900

    fix: 로그인하지 않은 유저가 장바구니 및 구매하기 기능 사용하려고 할 때 redirect 처리

--- apps/commerce-client-web/src/app/(default)/cart/components/cart-add-button.tsx
+++ apps/commerce-client-web/src/app/(default)/cart/components/cart-add-button.tsx
@@ -5,8 +5,9 @@ import { Button } from '@/components/ui/button';
 import { ToastAction } from '@/components/ui/toast';
 import Link from 'next/link';
 import { Product } from '@/types/product-types';
-import { useLoading } from '@/components/common/loading-context'; // useLoading 훅 임포트
+import { useLoading } from '@/components/common/loading-context';
 import useCartStore from '@/stores/use-cart-store';
+import { useAuthStore } from '@/stores/use-auth-store';
 
 interface AddToCartButtonProps {
   book: Product;
@@ -17,19 +18,39 @@ const AddToCartButton = ({ book, quantity }: AddToCartButtonProps) => {
   const { toast } = useToast();
   const { setLoading } = useLoading();
   const { addItemToCart } = useCartStore();
+  const { isLoggedIn } = useAuthStore();
 
   const handleAddToCart = async (e: { stopPropagation: () => void }) => {
     e.stopPropagation();
+
+    if (!isLoggedIn) {
+      // Show login prompt if user is not logged in
+      toast({
+        title: '로그인이 필요합니다',
+        description: '장바구니에 상품을 추가하려면 로그인이 필요합니다.',
+        action: (
+          <div className="mt-4 flex w-full justify-end">
+            <ToastAction altText="로그인 페이지로 이동" className="border-slate-600">
+              <Link href="/login">로그인</Link>
+            </ToastAction>
+          </div>
+        ),
+        duration: 3000,
+        className: 'max-w-2xl p-8 flex flex-col justify-between',
+      });
+      return;
+    }
+
     try {
       setLoading(true);
       await addItemToCart(book.id, quantity);
       toast({
-        title: 'Added to cart!',
-        description: `${book.title}이(가) ${quantity}개가 장바구니에 추가되었습니다.`,
+        title: '장바구니에 추가되었습니다!',
+        description: `${book.title}이(가) 장바구니에 추가되었습니다.`,
         action: (
           <div className="mt-4 flex w-full justify-end">
-            <ToastAction altText="바로가기" className="border-slate-600">
-              <Link href={'/cart'}>바로가기</Link>
+            <ToastAction altText="장바구니 보기" className="border-slate-600">
+              <Link href="/cart">장바구니 보기</Link>
             </ToastAction>
           </div>
         ),
@@ -38,13 +59,13 @@ const AddToCartButton = ({ book, quantity }: AddToCartButtonProps) => {
       });
     } catch (error) {
       toast({
-        title: 'Error',
-        description: `장바구니 추가에 실패했습니다. 다시 시도해주세요.`,
+        title: '오류 발생',
+        description: '장바구니에 상품을 추가하는 데 실패했습니다. 다시 시도해 주세요.',
         duration: 3000,
       });
-      console.error('Failed to add item to cart:', error);
+      console.error('장바구니 추가 실패:', error);
     } finally {
-      setLoading(false); // 로딩 종료
+      setLoading(false);
     }
   };
 
